ca0472e7c69a096d243d1d2ffee7f7edc1341a72
Roma038 <romalezgin1997@gmail.com>
2021-09-23 14:22:10 +0400

    Add project files.

---
+++ SpaceHunter/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SpaceHunter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hunter = new System.Windows.Forms.PictureBox();
+            this.Platform = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Hunter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hunter
+            // 
+            this.Hunter.BackColor = System.Drawing.Color.White;
+            this.Hunter.BackgroundImage = global::SpaceHunter.Properties.Resources.snippet;
+            this.Hunter.Image = global::SpaceHunter.Properties.Resources.bomber_sprite;
+            this.Hunter.Location = new System.Drawing.Point(340, 547);
+            this.Hunter.Margin = new System.Windows.Forms.Padding(0);
+            this.Hunter.Name = "Hunter";
+            this.Hunter.Size = new System.Drawing.Size(60, 50);
+            this.Hunter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Hunter.TabIndex = 1;
+            this.Hunter.TabStop = false;
+            this.Hunter.Tag = "Hunter";
+            // 
+            // Platform
+            // 
+            this.Platform.BackColor = System.Drawing.Color.OliveDrab;
+            this.Platform.Image = global::SpaceHunter.Properties.Resources.fly_ground;
+            this.Platform.Location = new System.Drawing.Point(0, 600);
+            this.Platform.Name = "Platform";
+            this.Platform.Size = new System.Drawing.Size(782, 246);
+            this.Platform.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Platform.TabIndex = 0;
+            this.Platform.TabStop = false;
+            this.Platform.Tag = "Platform";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SpaceHunter.Properties.Resources.star_sky;
+            this.ClientSize = new System.Drawing.Size(782, 653);
+            this.Controls.Add(this.Hunter);
+            this.Controls.Add(this.Platform);
+            this.Name = "Form1";
+            this.Text = "SPACE HUNTER";
+            this.Load += new System.EventHandler(this.Start_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown_Hunter);
+            ((System.ComponentModel.ISupportInitialize)(this.Hunter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Platform;
+        private System.Windows.Forms.PictureBox Hunter;
+    }
+}
+
---
+++ SpaceHunter/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace SpaceHunter
+{
+    public partial class Form1 : Form
+    {
+        private bool left = false;
+        private bool right = false;
+        private bool shooting = false;
+        List<PictureBox> _bullets = new List<PictureBox>();
+        List<PictureBox> _asteroids = new List<PictureBox>();
+        private BackgroundWorker backgroundWorker;
+        int X = 10;
+        //int Y = 540;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.backgroundWorker = new BackgroundWorker();
+            this.backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Asteroids();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //MoveHinter();
+            //Create_Bullet();
+            foreach (var item in _bullets)
+            {
+                item.Top -= 15;
+            }
+        }
+
+        private void Start_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 10;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            System.Windows.Forms.Timer asteroids_timer = new System.Windows.Forms.Timer();
+            asteroids_timer.Interval = 300;
+            asteroids_timer.Tick += Asteroids_timer_Tick;
+            asteroids_timer.Start();
+
+        }
+
+        private void Asteroids_timer_Tick(object sender, EventArgs e)
+        {
+            //Thread thread = new Thread(Asteroids);
+            //thread.IsBackground = true;
+            //thread.Start();
+            //Asteroids();
+            backgroundWorker.RunWorkerAsync();
+            foreach (var item in _asteroids)
+            {
+                item.Top += 10;
+            }
+        }
+
+        private void Create_Bullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.BackColor = Color.OrangeRed;
+            bullet.Height = 30;
+            bullet.Width = 10;
+            bullet.Location = new Point(Hunter.Location.X + 18, Hunter.Location.Y);
+            bullet.Refresh();
+            if (shooting)
+            {
+                _bullets.Add(bullet);
+                this.Controls.Add(bullet);
+            }
+            
+
+        }
+
+        private void Asteroids()
+        {
+            PictureBox asteroid = new PictureBox();
+            asteroid.BackColor = Color.OrangeRed;
+            asteroid.Height = 30;
+            asteroid.Width = 30;
+            asteroid.ImageLocation = Path.Combine(Application.StartupPath, "Resources\\Asteroid.png");
+            asteroid.SizeMode = PictureBoxSizeMode.StretchImage;
+            //Image background = new Bitmap(Path.Combine(Application.StartupPath, "Resources\\snippet.png"));
+            //asteroid.BackgroundImage = background;
+            _asteroids.Add(asteroid);
+            Random random = new Random();
+            int rand_x = random.Next(30, 550);
+            asteroid.Left = rand_x;
+            asteroid.Refresh();
+            this.Controls.Add(asteroid);
+            
+            //Thread.Sleep(10);
+
+
+        }
+
+        private void MoveHinter()
+        {
+            if (left && Hunter.Left > 0)
+            {
+                Hunter.Left -= X;
+            }
+            if (right && Hunter.Left < 535)
+            {
+                Hunter.Left += X;
+            }
+        }
+
+        private void KeyDown_Hunter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+                right = false;
+                MoveHinter();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+                left = false;
+                MoveHinter();
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                shooting = true;
+                Create_Bullet();
+            }
+        }
+    }
+}
